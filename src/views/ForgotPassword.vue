b6daed9d905a27aabcd408e92db168cba2b25e1a
Gab Fiterman <gfiterman96@gmail.com>
2022-09-25 13:05:44 -0300

    Created Forgot Password View

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,15 +1,49 @@
 <template>
-  <div>
-    <h1>Forgot Password</h1>
+  <div class="reset-password">
+    <div class="form-wrap">
+      <form class="reset">
+        <h2>Reset password</h2>
+        <p>Forgot your password? Enter your email to reset it.</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email" />
+            <email class="icon" />
+          </div>
+        </div>
+
+        <button>Sign In</button>
+
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import email from "../assets/Icons/envelope-regular.svg";
+
 export default {
-name: 'ForgotPassword',
-}
+  name: "ForgotPassword",
+  components:{
+    email,
+  },
+};
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss" scoped>
+  .reset-password {
+    position: relative;
+    .form-wrap {
+      .reset {
+        h2 {
+          margin-bottom: 8px;
+        }
+        p {
+          text-align: center;
+          margin-bottom: 32px;
+        }
+      }
+    }
+  }
+  </style>
