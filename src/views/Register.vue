68f049de0a2d842c7be26e6902ed090a98222df0
Gab Fiterman <gfiterman96@gmail.com>
2022-09-25 12:55:21 -0300

    Created Register View

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,15 +1,76 @@
 <template>
-  <div>
-    <h1>Register</h1>
+  <div class="form-wrap">
+    <form action="" class="register">
+      <p class="login-register">
+        Already have an account ?
+        <router-link class="router link" :to="{ name: 'Login' }">
+          Register
+        </router-link>
+      </p>
+      <h2>Create Your FireBlogs Account</h2>
+      <div class="inputs">
+        <div class="input">
+          <input type="text" placeholder="Fist Name" v-model="firstName" />
+          <user class="icon" />
+        </div>
+
+        <div class="input">
+          <input type="text" placeholder="Last Name" v-model="lastName" />
+          <user class="icon" />
+        </div>
+
+        <div class="input">
+          <input type="text" placeholder="Username" v-model="username" />
+          <user class="icon" />
+        </div>
+
+        <div class="input">
+          <input type="text" placeholder="Email" v-model="email" />
+          <email class="icon" />
+        </div>
+
+        <div class="input">
+          <input type="password" placeholder="Password" v-model="password" />
+          <password class="icon" />
+        </div>
+      </div>
+
+      <button>Sign Up</button>
+
+      <div class="angle"></div>
+    </form>
+    <div class="background"></div>
   </div>
 </template>
 
 <script>
+import email from "../assets/Icons/envelope-regular.svg";
+import password from "../assets/Icons/lock-alt-solid.svg";
+import user from "../assets/Icons/user-alt-light.svg";
+
 export default {
-name: 'Register',
-}
+  name: "Register",
+  components: {
+    email,
+    password,
+    user,
+  },
+  data(){
+    return{
+      firstName: null,
+      lastName: null,
+      username: null,
+      email: null,
+      password: null,
+    };
+  },
+};
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+.register {
+  h2 {
+    max-width: 350px;
+  }
+}
 </style>
