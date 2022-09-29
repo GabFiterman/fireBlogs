44a9c6d35f90c63d555a752227ec3f9fe35742e5
Gab Fiterman <gfiterman96@gmail.com>
2022-09-29 18:28:20 -0300

    Front-End/UserMenu: Added basic and functional user menu

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,15 +5,69 @@
         <router-link class="header" :to="{ name: 'Home' }"
           >FireBlogs</router-link
         >
-        <h3 style="margin-left: 1rem; background-color: rgba(0,0,0,0.5); color: white; padding: 5px">{{this.$store.state.profileFirstName}} {{this.$store.state.profileLastName}}</h3>
+        <h3
+          v-if="user"
+          style="margin-left: 1rem; 
+            background-color: rgba(154, 4, 234, 0.45); 
+            color: white; 
+            padding: 5px;
+            border-radius: 4px;
+            box-shadow: var(main-box-);"
+        >
+          {{ this.$store.state.profileFirstName }}
+          {{ this.$store.state.profileLastName }}
+        </h3>
       </div>
       <div class="nav-links">
         <ul v-if="!mobile">
           <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
           <router-link class="link" :to="{ name: 'Blogs' }">Blogs</router-link>
           <router-link class="link" to="#">Create Post</router-link>
-          <router-link class="link" :to="{ name: 'Login' }">Login/Register</router-link>
+          <router-link class="link" :to="{ name: 'Login' }"
+            >Login/Register</router-link
+          >
         </ul>
+        <div
+          v-if="user"
+          @click="toggleProfileMenu"
+          class="profile"
+          ref="profile"
+        >
+          <span>{{ this.$store.state.profileInitials }}</span>
+          <div v-show="profileMenu" class="profile-menu">
+            <div class="info">
+              <p class="initials">{{ this.$store.state.profileInitials }}</p>
+              <div class="right">
+                <p>
+                  {{ this.$store.state.profileFirstName }}
+                  {{ this.$store.state.profileLastName }}
+                </p>
+                <p>{{ this.$store.state.profileUsername }}</p>
+                <p>{{ this.$store.state.profileEmail }}</p>
+              </div>
+            </div>
+            <div class="options">
+              <div class="option">
+                <router-link class="option" to="#">
+                  <userIcon class="icon" />
+                  <p>Profile</p>
+                </router-link>
+              </div>
+
+              <div class="option">
+                <router-link class="option" to="#">
+                  <adminIcon class="icon" />
+                  <p>Admin</p>
+                </router-link>
+              </div>
+
+              <div @click="signOut" class="option">
+                <signOutIcon class="icon" />
+                <p>Sign Out</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
     <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile" />
@@ -22,7 +76,9 @@
         <router-link class="link" :to="{ name: 'Home' }">Home</router-link>
         <router-link class="link" :to="{ name: 'Blogs' }">Blogs</router-link>
         <router-link class="link" to="#">Create Post</router-link>
-        <router-link class="link" :to="{ name: 'Login' }">Login/Register</router-link>
+        <router-link class="link" :to="{ name: 'Login' }"
+          >Login/Register</router-link
+        >
       </ul>
     </transition>
   </header>
@@ -30,23 +86,34 @@
 
 <script>
 import menuIcon from "../assets/Icons/bars-regular.svg";
+import userIcon from "../assets/Icons/user-alt-light.svg";
+import adminIcon from "../assets/Icons/user-crown-light.svg";
+import signOutIcon from "../assets/Icons/sign-out-alt-regular.svg";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 export default {
   name: "navigation",
   components: {
     menuIcon,
+    userIcon,
+    adminIcon,
+    signOutIcon,
   },
   data() {
     return {
+      profileMenu: false,
       mobile: null,
       mobileNav: null,
       windowWidth: null,
     };
   },
+
   created() {
     window.addEventListener("resize", this.checkScreen);
     this.checkScreen();
   },
+
   methods: {
     // check if the screen size is mobile or not, for correct display of the nav menu
     checkScreen() {
@@ -63,6 +130,23 @@ export default {
     toggleMobileNav() {
       this.mobileNav = !this.mobileNav;
     },
+
+    toggleProfileMenu(e) {
+      if (e.target === this.$refs.profile) {
+        this.profileMenu = !this.profileMenu;
+      }
+    },
+
+    signOut() {
+      firebase.auth().signOut();
+      window.location.reload();
+    },
+  },
+
+  computed: {
+    user() {
+      return this.$store.state.user;
+    },
   },
 };
 </script>
@@ -71,8 +155,7 @@ export default {
 header {
   background-attachment: #fff;
   padding: 0 25px;
-  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  box-shadow: var(main-box-shadow);
   z-index: 99;
 
   .link {
@@ -116,6 +199,91 @@ header {
 
         .link:last-child {
           margin-right: 0;
+        }
+      }
+
+      .profile {
+        position: relative;
+        cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        color: #fff;
+        background-color: #303030;
+
+        span {
+          pointer-events: none;
+        }
+
+        .profile-menu {
+          position: absolute;
+          top: 60px;
+          right: 0;
+          width: 250px;
+          background-color: #303030;
+          box-shadow: var(main-box-shadow);
+
+          .info {
+            display: flex;
+            align-items: center;
+            padding: 15px;
+            border-bottom: 1px solid #fff;
+
+            .initials {
+              position: initial;
+              width: 40px;
+              height: 40px;
+              color: #303030;
+              background-color: #fff;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              border-radius: 50%;
+            }
+
+            .right {
+              flex: 1;
+              margin-left: 24px;
+
+              p:nth-child(1) {
+                font-size: 14px;
+              }
+
+              p:nth-child(2),
+              p:nth-child(3) {
+                font-size: 12px;
+              }
+            }
+          }
+
+          .options {
+            padding: 15px;
+
+            .option {
+              text-decoration: none;
+              color: #fff;
+              display: flex;
+              align-items: center;
+              margin-bottom: 12px;
+
+              .icon {
+                width: 18px;
+                height: auto;
+              }
+
+              p {
+                font-size: 14px;
+                margin-left: 12px;
+              }
+
+              &:last-child {
+                margin-bottom: 0px;
+              }
+            }
+          }
         }
       }
     }
